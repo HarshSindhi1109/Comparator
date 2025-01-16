2b94468e7e9d30588577da8a9689e121aca58d14
HarshSindhi1109 <harshsindhi085@gmail.com>
2025-01-16 23:22:06 +0530

    Adding new file Name.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,5 +16,15 @@ public class Main {
         System.out.println(names);
         names.sort(null);
         System.out.println(names);
+
+        List<Object> elements = new LinkedList<>();
+
+        elements.add(new Name("Superman"));
+        elements.add(new Name("Batman"));
+        elements.add(new Name("Ironman"));
+        elements.add(100);
+        elements.add(3.14);
+
+        System.out.println(elements);
     }
 }
